f89f1ca5be46d818a183b8b453ba8a5e7754376f
yashaswi maharshi <maha.yash@gmail.com>
2018-08-21 23:32:14 +0530

    fresh commit

---
+++ fe_taxnews/src/components/headerComp/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import headerStyle from './header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Menu Header</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
