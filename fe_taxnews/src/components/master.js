f4e2f3c9aa7c8f71fb8d240b61cfaee6418006eb
yashaswi maharshi <maha.yash@gmail.com>
2018-08-23 19:39:48 +0530

    mock data and show the data on the page

--- fe_taxnews/src/components/master.js
+++ fe_taxnews/src/components/master.js
@@ -1,26 +1,49 @@
 import React, { Component } from "react";
 import { browserHistory } from "react-router";
+import topStoriesData from "./masterArticleList.json";
+import incomeTaxData from "./incomeTax.json";
 
 //Navigation of all the Menu
 
 class Master extends Component {
   constructor(props) {
     super(props);
-    alert(this.props.location.pathname);
-    
+    ///alert(this.props.location.pathname);
   }
+
   state = {
-    urlParam: null
+    masterListData: []
   };
 
   componentDidMount() {
-    
+    // fetch the data as per the URL
+    // mean while will mock the json
+
+    switch (this.props.location.pathname) {
+      case "/top-stories":
+        this.setState({
+          masterListData: topStoriesData
+        });
+
+        break;
+      case "/income-tax":
+        this.setState({
+          masterListData: incomeTaxData
+        });
+        break;
+    }
   }
 
   render() {
     return (
       <div>
         <h1>Welcome to Master</h1>
+
+        <ul>
+          {this.state.masterListData.map(function(data) {
+            return <li>{data.title}</li>;
+          })}
+        </ul>
       </div>
     );
   }
