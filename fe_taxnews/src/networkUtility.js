a3685725af83eecbc2ec2c9baa0ee05d090180aa
yashaswi maharshi <maha.yash@gmail.com>
2018-08-28 20:29:32 +0530

    remove the comment

--- fe_taxnews/src/networkUtility.js
+++ fe_taxnews/src/networkUtility.js
@@ -3,9 +3,3 @@ export const HOME = "home";
 export const TOPSTORIES = "1";
 export const INCOMETAX = "2";
 
-// export const networkAPI = {
-//   URL: "localhost:4001/api/",
-//   home: "home",
-//   topStories: "1",
-//   incomeTax: "2"
-// };
