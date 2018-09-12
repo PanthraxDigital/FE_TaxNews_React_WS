37f27cf206bfc897f1454ba4c2233aab4f7b6f91
yashaswi maharshi <maha.yash@gmail.com>
2018-09-12 19:41:08 +0530

    change the API pointing location

--- fe_taxnews/src/networkUtility.js
+++ fe_taxnews/src/networkUtility.js
@@ -1,5 +1,5 @@
-//export const URL = "http://taxnewsback.squadl.com:4001/api/";
-export const URL = "http://localhost:4001/api/";
+export const URL = "http://www.taxknowledge.in:4001/api/";
+//export const URL = "http://localhost:4001/api/";
 export const HOME = "home";
 export const TOPSTORIES = "1";
 export const INCOMETAX = "2";
