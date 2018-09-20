c8d22986be2cafbce7f4c0173b80ef4be591b1de
yashaswi maharshi <maha.yash@gmail.com>
2018-09-20 18:20:54 +0530

    change the color of the ticker

--- fe_taxnews/src/components/newsTicker.js
+++ fe_taxnews/src/components/newsTicker.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import _ from "lodash";
 import { getCategory } from "../commonUtility";
+import styled from "styled-components";
 
 class NewsTicker extends React.Component {
   constructor(props) {
@@ -31,21 +32,66 @@ class NewsTicker extends React.Component {
     }
   }
   render() {
+    const TickerWrap = styled.div`
+      width: 8%;
+      overflow: hidden;
+      padding-right: 0px;
+      height: 4rem;
+      background-color: white;
+      padding-left: 91%;
+      box-sizing: content-box;
+    `;
+
+    const Ticker = styled.div`
+      display: inline-block;
+      height: 4rem;
+      line-height: 4rem;
+      white-space: nowrap;
+      padding-right: 100%;
+      box-sizing: content-box;
+      -webkit-animation-iteration-count: infinite;
+      animation-iteration-count: infinite;
+      -webkit-animation-timing-function: linear;
+      animation-timing-function: linear;
+      -webkit-animation-name: ticker;
+      animation-name: ticker;
+      -webkit-animation-duration: 150s;
+      animation-duration: 150s;
+    `;
+
+    const TickerItem = styled.div`
+      display: inline-block;
+      padding: 0 2rem;
+      font-size: 2rem;
+      color: #cf0000;
+      font-family: "Times New Roman", Times, serif;
+    `;
+
+    // const colorTickerItem = styled.span`
+    //   color: red;
+    //   font-size: 20px;
+
+    //   :hover {
+    //     text-decoration: none;
+    //   }
+    // `;
+
     return (
-      <div className="ticker-wrap container">
-        <div className="ticker">
+      <TickerWrap className="container">
+        <Ticker>
           {this.state.tickerData.map((data, index1) => (
-            <div className="ticker__item" key={index1}>
+            <TickerItem key={index1}>
               <Link
                 to={`${getCategory(parseInt(data.category))}/${data.id}`}
                 key={index1}
               >
-                {data.title}
+                <span>{data.title}</span>
               </Link>
-            </div>
+              
+            </TickerItem>
           ))}
-        </div>
-      </div>
+        </Ticker>
+      </TickerWrap>
     );
   }
 }
