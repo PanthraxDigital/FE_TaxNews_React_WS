6d1190cf706e5df50e740b2260d22dfcc19c6bee
yashaswi maharshi <maha.yash@gmail.com>
2018-09-24 08:00:11 +0530

    change the URL

--- fe_taxnews/src/networkUtility.js
+++ fe_taxnews/src/networkUtility.js
@@ -1,3 +1,3 @@
-//export const URL = "http://api.taxknowledge.in:4002/api/";
-export const URL = "http://localhost:4002/api/";
+export const URL = "http://api.taxknowledge.in:4002/api/";
+//export const URL = "http://localhost:4002/api/";
 export const HOME = "home";
