671fb13b1ada5b6b7b55c20d3766a6d1fdc9f5e5
yashaswi maharshi <maha.yash@gmail.com>
2018-11-20 09:24:20 +0530

    build for header justify and media share

--- fe_taxnews/src/networkUtility.js
+++ fe_taxnews/src/networkUtility.js
@@ -1,3 +1,4 @@
 export const URL = "http://api.taxknowledge.in:4002/api/";
+export const URL_X = "http://api.taxknowledge.in:4002/";
 //export const URL = "http://localhost:4002/api/";
 export const HOME = "home";
