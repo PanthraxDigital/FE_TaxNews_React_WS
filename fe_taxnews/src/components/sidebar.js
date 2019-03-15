ba511c479210ea609531834b36ea8669908ddf55
yashaswi maharshi <maha.yash@gmail.com>
2019-03-15 23:50:14 +0530

    added google ads

--- fe_taxnews/src/components/sidebar.js
+++ fe_taxnews/src/components/sidebar.js
@@ -20,35 +20,35 @@ class Sidebar extends Component {
 
   componentDidMount() {
     // show bitvertiser ads-1
-    (function(d) {
-      var params = {
-        bvwidgetid: "ntv_1986077",
-        bvlinksownid: 1986077,
-        rows: 2,
-        cols: 1,
-        textpos: "below",
-        imagewidth: 200,
-        mobilecols: 1,
-        cb: new Date().getTime()
-      };
-      params.bvwidgetid = "ntv_1986077" + params.cb;
-      d.getElementById("ntv_1986077").id = params.bvwidgetid;
-      var qs = Object.keys(params)
-        .reduce(function(a, k) {
-          a.push(k + "=" + encodeURIComponent(params[k]));
-          return a;
-        }, [])
-        .join(String.fromCharCode(38));
-      var s = d.createElement("script");
-      s.type = "text/javascript";
-      s.async = true;
-      var p = "https:" == document.location.protocol ? "https" : "http";
-      s.src =
-        p +
-        "://bvadtgs.scdn1.secure.raxcdn.com/bidvertiser/tags/active/bdvws.js?" +
-        qs;
-      d.getElementById(params.bvwidgetid).appendChild(s);
-    })(document);
+    // (function(d) {
+    //   var params = {
+    //     bvwidgetid: "ntv_1986077",
+    //     bvlinksownid: 1986077,
+    //     rows: 2,
+    //     cols: 1,
+    //     textpos: "below",
+    //     imagewidth: 200,
+    //     mobilecols: 1,
+    //     cb: new Date().getTime()
+    //   };
+    //   params.bvwidgetid = "ntv_1986077" + params.cb;
+    //   d.getElementById("ntv_1986077").id = params.bvwidgetid;
+    //   var qs = Object.keys(params)
+    //     .reduce(function(a, k) {
+    //       a.push(k + "=" + encodeURIComponent(params[k]));
+    //       return a;
+    //     }, [])
+    //     .join(String.fromCharCode(38));
+    //   var s = d.createElement("script");
+    //   s.type = "text/javascript";
+    //   s.async = true;
+    //   var p = "https:" == document.location.protocol ? "https" : "http";
+    //   s.src =
+    //     p +
+    //     "://bvadtgs.scdn1.secure.raxcdn.com/bidvertiser/tags/active/bdvws.js?" +
+    //     qs;
+    //   d.getElementById(params.bvwidgetid).appendChild(s);
+    // })(document);
 
     axios
       .get(URL_GROUP_JOIN)
