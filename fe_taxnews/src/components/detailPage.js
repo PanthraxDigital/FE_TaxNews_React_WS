4d2860273e12fed4736cc76a3197a480f9bca24f
yashaswi maharshi <maha.yash@gmail.com>
2019-04-05 11:16:04 +0530

    text align

--- fe_taxnews/src/components/detailPage.js
+++ fe_taxnews/src/components/detailPage.js
@@ -212,7 +212,11 @@ class DetailPage extends Component {
                       <div>
                         <div
                           className="mdFormat"
-                          style={{ fontSize: "1.1em", paddingTop: "50px" }}
+                          style={{
+                            fontSize: "1.1em",
+                            paddingTop: "50px",
+                            textAlign: "justify"
+                          }}
                           dangerouslySetInnerHTML={{
                             __html: this.descTop(
                               this.state.detailArticle[0].description
@@ -228,7 +232,11 @@ class DetailPage extends Component {
                         />
                         <div
                           className="mdFormat"
-                          style={{ fontSize: "1.1em", paddingTop: "50px" }}
+                          style={{
+                            fontSize: "1.1em",
+                            paddingTop: "50px",
+                            textAlign: "justify"
+                          }}
                           dangerouslySetInnerHTML={{
                             __html: this.descBottom(
                               this.state.detailArticle[0].description
