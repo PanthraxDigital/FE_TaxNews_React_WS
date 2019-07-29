41ad9881a04d04fe4abb2ab22653a80fe029cd25
yashaswi maharshi <maha.yash@gmail.com>
2019-07-30 03:01:25 +0530

    ads release build 3

--- fe_taxnews/src/components/footer.js
+++ fe_taxnews/src/components/footer.js
@@ -52,7 +52,7 @@ class Footer extends Component {
           <AdSense.Google
                 client="ca-pub-4354135056247821"
                 slot="1483284168"
-                
+                format='fluid'
               />
         </div>
       </div>
